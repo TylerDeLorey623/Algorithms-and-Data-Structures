b9850c1ba6e3fb2ddc3894fee646f4b6aece0fcf
TylerDeLorey623 <112398227+TylerDeLorey623@users.noreply.github.com>
2024-09-07 17:33:19 -0400

    File IO for magic items

--- Assignment 1/main.cpp
+++ Assignment 1/main.cpp
@@ -1,7 +1,36 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
 
 using namespace std;
 
 int main() {
-    cout << "Hello World!" << endl;
+    // Variable declaration
+    vector<string> magicItems;
+
+    string line;
+    
+    // File IO
+    ifstream file;
+
+    // Open file
+    file.open("magicitems.txt");
+    if (!file.is_open())
+    {
+        cout << "File failed to open." << endl;
+        return 1;
+    }
+
+    // Read from file
+    for (int i = 0; getline(file, line); i++)
+    {
+        magicItems.push_back(line);
+    }
+
+    // Close file
+    file.close();
+
+    // Print array
+    cout << magicItems[51] << endl;
+    cout << magicItems.size() << endl;
 }
