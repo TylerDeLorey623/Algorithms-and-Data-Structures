a2589e8aa3c19dfc4c941f46cbdfdb057c6077d2
TylerDeLorey623 <112398227+TylerDeLorey623@users.noreply.github.com>
2024-09-30 03:04:59 -0400

    Added quick sort functionality

--- Assignment 1/Sorts.h
+++ Assignment 1/Sorts.h
@@ -99,7 +99,7 @@ int insertionSort(vector<string> &myItems)
     return comparisonNum;
 }
 
-
+// Merges left and right arrays into a larger sorted array
 int merge(vector <string> &myItems, vector<string> &left, vector<string> &right, int m)
 {
     int comparisonNum = 0;
@@ -176,6 +176,82 @@ int mergeSort(vector<string> &myItems)
 
     // Conquers each section
     comparisonNum += merge(myItems, leftArray, rightArray, mid);
+
+    return comparisonNum;
+}
+
+// Returns both the partition point and the comparison count in a pair
+pair<int, int> partition(vector<string> &myItems, int p, int begin, int end)
+{
+    int comparisonNum = 0;
+
+    // Indicates the currently found pivot position 
+    int l = begin - 1;
+
+    // Swap myItems[p] and myItems[end] (moves pivot value to the end)
+    string temp = myItems[p];
+    myItems[p] = myItems[end];
+    myItems[end] = temp;
+
+    for (int i = begin; i < end; i++)
+    {
+        comparisonNum++;
+
+        // If current element is less than pivot element
+        if (myItems[i] < myItems[end])
+        {
+            // Swap myItems[l] and myItems[i] to move smaller element to the left partition
+            l++;
+
+            temp = myItems[l];
+            myItems[l] = myItems[i];
+            myItems[i] = temp; 
+        }
+    }
+
+    // Swap the pivot and partition indexes to put pivot in the correct position
+    temp = myItems[end];
+    myItems[end] = myItems[l + 1];
+    myItems[l + 1] = temp; 
+
+    return make_pair(l + 1, comparisonNum);
+}
+
+// Returns number of comparisons
+// Pass by reference (changes to myItems affects magicItems)
+int quickSort(vector<string> &myItems, int begin = 0, int end = INT_MIN)
+{
+    int comparisonNum = 0;
+    int size = myItems.size();
+
+    // If end parameter wasn't clarified, set it to the last index of the array (myItems[end] returns last element)
+    // This means the function is dealing with the entire list
+    if (end == INT_MIN)
+    {
+        end = size - 1;
+    }
+    // Base case
+    else if (begin >= end)
+    {
+        return comparisonNum;
+    }
+
+    // Selects random index for the partition
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<> distrib(begin, end);
+    int randomPivot = distrib(gen);
+
+    // Choosing the pivot element to be myItems[randomPivot]
+    auto parResult = partition(myItems, randomPivot, begin, end);
+
+    // Gets pivot index and comparison num
+    int r = parResult.first;
+    comparisonNum += parResult.second;
+
+    // Runs quick sort on first and second parts of the array
+    comparisonNum += quickSort(myItems, begin, r - 1);
+    comparisonNum += quickSort(myItems, r + 1, end);
 
     return comparisonNum;
 }
