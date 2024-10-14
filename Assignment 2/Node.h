0552cb13c4a383e80d1c305874c8405ee6a765b0
TylerDeLorey623 <112398227+TylerDeLorey623@users.noreply.github.com>
2024-10-14 01:08:38 -0400

    Added Node class for a linked list

---
+++ Assignment 2/Node.h
@@ -0,0 +1,27 @@
+// COPIED FROM ASSIGNMENT 1 (Node.h)
+#ifndef NODE_H
+#define NODE_H
+
+#include <iostream>
+
+using namespace std;
+
+// Public Node class for hash chaining
+class Node 
+{
+    public:
+        // Holds a single character for this node
+        string data;
+
+        // Pointer that points to the next node
+        Node* next;
+
+        // Declares parameterized constructor
+        Node(string value)
+        {
+            this->data = value;
+            this->next = nullptr;
+        }
+};
+
+#endif
