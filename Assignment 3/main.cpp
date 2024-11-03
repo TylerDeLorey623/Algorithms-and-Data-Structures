ed2b7c53a8cbefca4fa65358a25b00489b9e686e
TylerDeLorey623 <112398227+TylerDeLorey623@users.noreply.github.com>
2024-11-03 01:57:21 -0500

    Added file search functionality with BST and average comparison counting

--- Assignment 3/main.cpp
+++ Assignment 3/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <iomanip>
 
 #include "BST.h"
 
@@ -8,10 +9,14 @@ using namespace std;
 int main()
 {
     // BST Declaration
-    BST myTree; 
+    BST myTree;
+
+    int comparisonNum = 0;
+    float avg = 0;
+    int itemCount = 0;
 
     // File IO
-    string line;
+    string item;
     ifstream file;
 
     // Open magicitems file
@@ -23,9 +28,9 @@ int main()
     }
 
     // Read each line from file and insert it into the BST
-    while (getline(file, line))
+    while (getline(file, item))
     {
-        myTree.insert(line);
+        myTree.insert(item);
     }
 
     // Close magicitems file
@@ -33,5 +38,28 @@ int main()
 
     // Prints out entire BST with an in-order traversal
     cout << endl << "INORDER TRAVERSAL OF BST:" << endl;
-    myTree.inorderBST(myTree.root);
+    myTree.inorderTrav();
+
+    // Finding magicitems-find-in-bst.txt in the BST
+    file.open("magicitems-find-in-bst.txt");
+    if (!file.is_open())
+    {
+        cout << "File failed to open." << endl;
+        return 1;
+    }
+
+    // Read each line from file and search for it in the BST
+    while (getline(file, item))
+    {
+        cout << "Comparisons to find \"" << item << "\": ";
+        comparisonNum = myTree.search(item);
+        cout << "(" << comparisonNum << ")" << endl;
+        avg += comparisonNum;
+        itemCount++;
+    }
+
+    // Calculate average comparisons
+    avg = avg / itemCount;
+    cout << endl << "Average Number of Comparisons for BST Search: " << fixed << setprecision(2) << avg << endl << endl;
+
 }
