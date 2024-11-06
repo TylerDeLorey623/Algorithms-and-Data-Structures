4bd172bf72f5aec89e1ec364b1aa02d7cda4365e
TylerDeLorey623 <112398227+TylerDeLorey623@users.noreply.github.com>
2024-11-06 02:01:22 -0500

    Matrix function for Graph

--- Assignment 3/Graph.h
+++ Assignment 3/Graph.h
@@ -17,12 +17,14 @@ class Graph
         vector<Vertex> vertices;
         vector<Edge> edges;
 
+        // Add a Vertex to the Graph
         void addVertex(string vertexID)
         {
             Vertex newVertex = Vertex(vertexID);
             vertices.push_back(newVertex);
         }
 
+        // Finds a specific Vertex based on its ID
         Vertex* findVertex(string vertexID)
         {
             for (int i = 0, n = vertices.size(); i < n; i++)
@@ -36,6 +38,7 @@ class Graph
             throw runtime_error("Vertex not found: " + vertexID);
         }
 
+        // Adds an edge between two Vertices
         void addEdge(string v1, string v2)
         {
             Vertex* vertex1 = findVertex(v1);
@@ -45,6 +48,55 @@ class Graph
             edges.push_back(newEdge);
             vertex1->addEdgeToVertex(&newEdge);
             vertex2->addEdgeToVertex(&newEdge);
+        }
+
+        // Prints a matrix for the Graph
+        void printMatrix()
+        {
+            // Initialize matrix variables
+            int vertSize = vertices.size();
+            int edgeCount = edges.size();
+            string firstVertex, secondVertex;
+            int first = 0, second = 0;
+
+            // Initialize matrix 2D array
+            int matrix[vertSize][vertSize];
+
+            // Finds connected vertices
+            for (int iter = 0; iter < edgeCount; iter++)
+            {
+                firstVertex = edges[iter].vertex1->id;
+                secondVertex = edges[iter].vertex2->id;
+                first = stoi(firstVertex) - 1;
+                second = stoi(secondVertex) - 1;
+
+                matrix[first][second] = 1;
+                matrix[second][first] = 1;
+            }
+
+            // Prints entire Matrix
+            cout << "M ";
+            for (int i = 0; i < vertSize; i++)
+            {
+                cout << vertices[i].id << " ";
+            }
+            cout << endl;
+
+            for (int i = 0; i < vertSize; i++)
+            {
+                cout << vertices[i].id << " ";
+                for (int j = 0; j < vertSize; j++)
+                {
+                    if (matrix[i][j] != 1)
+                    {
+                        matrix[i][j] = 0;
+                    }
+                    
+                    cout << matrix[i][j] << " ";
+                }
+                cout << endl;
+            }
+
         }
 };
 
