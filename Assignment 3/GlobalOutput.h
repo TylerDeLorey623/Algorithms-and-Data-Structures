c0a79b5e29f2be676239774944a79686be047d7e
TylerDeLorey623 <112398227+TylerDeLorey623@users.noreply.github.com>
2024-11-11 17:21:50 -0500

    Created file to store all program output

---
+++ Assignment 3/GlobalOutput.h
@@ -0,0 +1,12 @@
+#ifndef GLOBALOUTPUT_H
+#define GLOBALOUTPUT_H
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+// Deals with sending all output from the files to one txt file (output.txt)
+ofstream outFile("output.txt");
+
+#endif
