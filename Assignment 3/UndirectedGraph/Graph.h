880135f33932444a38dbeec057fcfbd3b8a68e38
TylerDeLorey623 <112398227+TylerDeLorey623@users.noreply.github.com>
2024-11-13 09:28:25 -0500

    Renamed DFS/BFS to DFT/BFT, also added functions to check for any unconnected Vertices for traversals

--- Assignment 3/UndirectedGraph/Graph.h
+++ Assignment 3/UndirectedGraph/Graph.h
@@ -44,8 +44,66 @@ class Graph
             vertices[vertex2]->addNeighbor(vertices[vertex1]);
         }
 
+        // Calls a private function for depth first traversal starting from root Vertex
+        void depthFirstTrav()
+        {
+            DFT(vertices[0]);
+        }
+
+        // Does a DFT for any parts not connected to the root Vertex
+        void checkDFT()
+        {
+            for (int i = 0, n = vertices.size(); i < n; i++)
+            {
+                if (!vertices[i]->processed)
+                {
+                    DFT(vertices[i]);
+                }
+            }
+        }
+
+        // Calls a private function for breadth first traversal starting from root Vertex
+        void breadthFirstTrav()
+        {
+            BFT(vertices[0]);
+        }
+
+        // Does a BFT for any parts not connected to the root Vertex
+        void checkBFT()
+        {
+            for (int i = 0, n = vertices.size(); i < n; i++)
+            {
+                if (!vertices[i]->processed)
+                {
+                    BFT(vertices[i]);
+                }
+            }
+        }
+
+        void resetProcessed()
+        {
+            for (int i = 0, n = vertices.size(); i < n; i++)
+            {
+                vertices[i]->processed = false;
+            }
+        }
+
+        // Unloads each Vertex in the Graph
+        void unloadGraph()
+        {
+            // Deletes each Vertex in the Graph
+            for (int i = 0, n = vertices.size(); i < n; i++)
+            {
+                delete(vertices[i]);
+            }
+
+            // Clears the Vertices Array
+            vertices.clear();
+        }
+
+    private:
         // Does a Depth First Traversal of the graph
-        void DFS(Vertex* v)
+        void DFT(Vertex* v)
         {
             if (!v->processed)
             {
@@ -57,13 +115,13 @@ class Graph
             {
                 if (!v->neighbors[i]->processed)
                 {
-                    DFS(v->neighbors[i]);
+                    DFT(v->neighbors[i]);
                 }
             }
         }
 
         // Does a Breadth First Traversal of the graph
-        void BFS(Vertex* vertex)
+        void BFT(Vertex* vertex)
         {
             Vertex* currentVertex;
             Vertex* currentNeighbor;
@@ -88,27 +146,6 @@ class Graph
                 }
             }
             delete(q);
-        }
-
-        void resetProcessed()
-        {
-            for (int i = 0, n = vertices.size(); i < n; i++)
-            {
-                vertices[i]->processed = false;
-            }
-        }
-
-        // Unloads each Vertex in the Graph
-        void unloadGraph()
-        {
-            // Deletes each Vertex in the Graph
-            for (int i = 0, n = vertices.size(); i < n; i++)
-            {
-                delete(vertices[i]);
-            }
-
-            // Clears the Vertices Array
-            vertices.clear();
         }
 };
 
