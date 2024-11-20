f0eb84f92ed940bb1ebd8dc0342c4fb344e162b2
TylerDeLorey623 <112398227+TylerDeLorey623@users.noreply.github.com>
2024-11-20 18:55:21 -0500

    Added edges vector, which stores the two vertices and its weight

--- Assignment 4/Graph.h
+++ Assignment 4/Graph.h
@@ -12,8 +12,9 @@ using namespace std;
 class Graph 
 {
     public:
-        // Vectors of the Graph's Vertices
+        // Vectors of the Graph's Vertices and Edges
         vector<Vertex*> vertices;
+        vector< pair < pair < Vertex*, Vertex*>, int> > edges;
 
         // Add a Vertex to the Graph
         void addVertex(string vertexID)
@@ -36,10 +37,18 @@ class Graph
             throw runtime_error("Vertex not found: " + vertexID);
         }
 
-        // Adds an edge between two Vertices (parameters are indices in vertices Array)
-        void addEdge(int vertex1, int vertex2, int weight)
+        // Adds an edge from Vertex 1 to Vertex 2
+        void addEdge(string vertex1, string vertex2, int weight)
         {
-            vertices[vertex1]->addNeighbor(vertices[vertex2], weight);
+            // Gets the index of these Vertices in the vertices array
+            int vertex1Index = findVertexIndex(vertex1);
+            int vertex2Index = findVertexIndex(vertex2);
+
+            Vertex* v1 = vertices[vertex1Index];
+            Vertex* v2 = vertices[vertex2Index];
+
+            // Adds an edge to the edges vector (including the weight)
+            edges.push_back({{v1, v2}, weight});
         }
 
         // Unloads each Vertex in the Graph
